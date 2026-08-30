aa2d8725df9b8c684f4ca9c279b3120b1583b830
kmhswimgirl <kmherbstzuber@wpi.edu>
2026-08-29 22:59:24 -0400

    removed dead code

--- launch/windows.launch.py
+++ launch/windows.launch.py
@@ -20,19 +20,14 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import AppendEnvironmentVariable, DeclareLaunchArgument, SetEnvironmentVariable
+from launch.actions import SetEnvironmentVariable
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+
 
 
 def generate_launch_description():
-    launch_file_dir = os.path.join(get_package_share_directory('pushback_sim'), 'launch')
     ros_gz_sim = get_package_share_directory('ros_gz_sim')
-
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    #x_pose = LaunchConfiguration('x_pose', default='-2.0')
-    #y_pose = LaunchConfiguration('y_pose', default='-0.5')
 
     world = os.path.join(
         get_package_share_directory('pushback_sim'),
@@ -54,23 +49,6 @@ def generate_launch_description():
         launch_arguments={'gz_args': '-g -v2 ', 'on_exit_shutdown': 'true'}.items()
     )
 
-#    robot_state_publisher_cmd = IncludeLaunchDescription(
-#        PythonLaunchDescriptionSource(
-#            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
-#        ),
-#        launch_arguments={'use_sim_time': use_sim_time}.items()
-#    )
-
-#    spawn_turtlebot_cmd = IncludeLaunchDescription(
-#        PythonLaunchDescriptionSource(
-#            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
-#        ),
-#        launch_arguments={
-#            'x_pose': x_pose,
-#            'y_pose': y_pose
-#        }.items()
-#    )
-
     # base file path for the package
     vex_path = os.path.join(get_package_share_directory('pushback_sim'))
 
@@ -84,20 +62,11 @@ def generate_launch_description():
         value=f"{models_path}:{worlds_path}:{vex_path}"
     )
 
-    #set_env_vars_resources = AppendEnvironmentVariable(
-    #        'GZ_SIM_RESOURCE_PATH',
-    #        os.path.join(
-    #            get_package_share_directory('pushback_sim'),
-    #            'models'))
-
     ld = LaunchDescription()
 
     # Add the commands to the launch description
     ld.add_action(gz_sim_resource)
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
-    #ld.add_action(spawn_turtlebot_cmd)
-    #ld.add_action(robot_state_publisher_cmd)
-    #ld.add_action(set_env_vars_resources)
 
     return ld
